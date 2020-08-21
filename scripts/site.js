37148ccc9cedcfbe5a02ec422a93875d5a69e551
Dan C# <csharpster@gmail.com>
2020-08-21 00:18:54 -0400

    Initial commit

---
+++ scripts/site.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $('#incrementCounter').click(function() {
+        $.getJSON("https://api.countapi.xyz/hit/dc-fearless-trial/1ccb732e-b55a-4404-ad3f-0f99c02fe44e", function(response) {
+            $("#counter").text(response.value);
+        });    
+    })
+
+  });
